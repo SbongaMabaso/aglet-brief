cd33de53ec68515e13c90c9fb6fd35f3036d50ce
Sbonga Mabaso <sbongamabaso5@gmail.com>
2022-02-09 21:35:03 -0800

    one

---
+++ app.js
@@ -0,0 +1,13 @@
+$(function ()
+    {
+        $('.multiple-items').slick({
+            dots: false,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            arrows: false,
+            scrollbars: true,
+        });
+    }
+);
